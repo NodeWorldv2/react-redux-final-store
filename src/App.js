17aae3bc52e29d977209551f1be0d00be5c0ac6b
Adil Reza <adilreza043@gmail.com>
2020-02-23 13:02:27 +0600

    adding simple redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
+import {createStore} from 'redux'
+
+class App extends Component {
+  render(){
+
+    const reducer = (state={},action) =>{
+      if(action.type==="A")
+      {
+        console.log("a is called");
+        
+        return {
+          ...state,
+          A: "Thi is A state called"
+        }
+
+      }
+      else if(action.type==="C")
+      {
+        return {
+          ...state,
+          B: "This is state B"
+        }
+      }
+      else
+      {
+
+      return state;
+      }
+
+    }
+    //already created a store 
+    const central_store = createStore(reducer)
+
+    //we have to use the subscriber to different component to access the central storage
+
+    central_store.subscribe(()=>{
+      console.log(central_store.getState())
+    })
+
+
+    central_store.dispatch({type:"A"})
+    central_store.dispatch({type:"B"})
+    central_store.dispatch({type:"C"})
+    central_store.dispatch({type:"D"})
+
+
+
+
+    
+    
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h5>clean app</h5>
+      <hr/>
     </div>
   );
+
+}
+  
 }
 
 export default App;
